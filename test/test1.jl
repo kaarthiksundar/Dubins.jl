f794b8b8b25684ca8b8a37951c2dec84ddd65974
kaarthiksundar <kaarthik01sundar@gmail.com>
2018-04-24 16:12:20 -0600

    mc_tests

--- test/test1.jl
+++
@@ -1,7 +0,0 @@
-@testset "test dubins shortest path length" begin
-    path = DubinsPath()
-    errcode = dubins_shortest_path(path, zeros(3), [5., 0., 3.], 1.)
-    @test errcode == 0
-    path_length = dubins_path_length(path)
-    @test isapprox(path_length, 8.2725, atol=1e-3)
-end
