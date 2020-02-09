e94588da474dec5f3db85361d451d2cd910cdff7
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2020-02-09 10:09:28 -0700

    clean up

--- src/Dubins.jl
+++ src/Dubins.jl
@@ -1,17 +1,23 @@
-isdefined(Base, :__precompile__) && __precompile__()
-
 module Dubins
 
 using Memento
-using Compat
-
-
-import Compat: @__MODULE__
 
 const LOGGER = getlogger(@__MODULE__)
-setlevel!(LOGGER, "info")
+Memento.setlevel!(LOGGER, "info")
 
 __init__() = Memento.register(LOGGER)
+
+"Suppresses information and warning messages output by Dubins, for fine grained control use the Memento package"
+function silence()
+    Memento.info(LOGGER, "Suppressing information and warning messages for the rest of this session.  Use the Memento package for more fine-grained control of logging.")
+    Memento.setlevel!(Memento.getlogger(Dubins), "error")
+end
+
+"alows the user to set the logging level without the need to add Memento"
+function logger_config!(level)
+    Memento.config!(Memento.getlogger("Dubins"), level)
+end
+
 
 include("typedefs.jl")
 include("paths.jl")
