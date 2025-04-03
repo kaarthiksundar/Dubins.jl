840b9df0e55ded005a327ec6ecd2f1a81ba384e1
Daniel Cherenson <dmrc@umich.edu>
2025-04-03 12:02:54 -0400

    added user specified loop tolerance

--- src/paths.jl
+++ src/paths.jl
@@ -1,5 +1,4 @@
-function cleanup_loop(x::F, tol = 1e-9) where {F}
-    # return x
+function cleanup_loop(x::F, tol::F) where {F}
     if x >= 2π - tol
         return zero(F)
     else
@@ -7,21 +6,18 @@ function cleanup_loop(x::F, tol = 1e-9) where {F}
     end
 end
 
-function dubins_LSL(intermediate_results::DubinsIntermediateResults{F}) where {F}
+function dubins_LSL(intermediate_results::DubinsIntermediateResults{F}, loop_tol::F) where {F}
 
     tmp0 = intermediate_results.d + intermediate_results.sa - intermediate_results.sb
     p_sq =
         2 + intermediate_results.d_sq - (2 * intermediate_results.c_ab) +
         (2 * intermediate_results.d * (intermediate_results.sa - intermediate_results.sb))
-    # tmp0 = round(tmp0; digits=10)
-    # p_sq = round(p_sq; digits=10)
 
     if p_sq >= 0
         tmp1 = atan((intermediate_results.cb - intermediate_results.ca), tmp0)
-        # tmp1 = round(tmp1; digits=10)
-        out1 = mod2pi(tmp1 - intermediate_results.α) |> cleanup_loop
+        out1 = cleanup_loop(mod2pi(tmp1 - intermediate_results.α), loop_tol)
         out2 = sqrt(p_sq)
-        out3 = mod2pi(intermediate_results.β - tmp1) |> cleanup_loop
+        out3 = cleanup_loop(mod2pi(intermediate_results.β - tmp1), loop_tol)    
         out = SVector{3,F}(out1, out2, out3)
         return EDUBOK, out
     end
@@ -29,21 +25,18 @@ function dubins_LSL(intermediate_results::DubinsIntermediateResults{F}) where {F
     return EDUBNOPATH, SVector{3,F}(0, 0, 0)
 end
 
-function dubins_RSR(intermediate_results::DubinsIntermediateResults{F}) where {F}
+function dubins_RSR(intermediate_results::DubinsIntermediateResults{F}, loop_tol::F) where {F}
 
     tmp0 = intermediate_results.d - intermediate_results.sa + intermediate_results.sb
     p_sq =
         2 + intermediate_results.d_sq - (2 * intermediate_results.c_ab) +
         (2 * intermediate_results.d * (intermediate_results.sb - intermediate_results.sa))
-    # tmp0 = round(tmp0; digits=10)
-    # p_sq = round(p_sq; digits=10)
 
     if p_sq >= 0
         tmp1 = atan((intermediate_results.ca - intermediate_results.cb), tmp0)
-        # tmp1 = round(tmp1; digits=10)
-        out1 = mod2pi(intermediate_results.α - tmp1) |> cleanup_loop
+        out1 = cleanup_loop(mod2pi(intermediate_results.α - tmp1), loop_tol)
         out2 = sqrt(p_sq)
-        out3 = mod2pi(tmp1 - intermediate_results.β) |> cleanup_loop
+        out3 = cleanup_loop(mod2pi(tmp1 - intermediate_results.β), loop_tol)
         out = SVector{3,F}(out1, out2, out3)
         return EDUBOK, out
     end
@@ -51,28 +44,13 @@ function dubins_RSR(intermediate_results::DubinsIntermediateResults{F}) where {F
     return EDUBNOPATH, SVector{3,F}(0, 0, 0)
 end
 
-# function mymod2pi(x)
-#     return atan(sin(x), cos(x))
-# end
-
-# function fmodr(x, y)
-#     return x - y*floor(x/y)
-# end
-
-# function mymod2pi( theta )
-
-#     return fmodr( theta, 2π)
-# end
-
-
-function dubins_LSR(intermediate_results::DubinsIntermediateResults{F}) where {F}
+function dubins_LSR(intermediate_results::DubinsIntermediateResults{F}, loop_tol::F) where {F}
 
     p_sq =
         -2 +
         (intermediate_results.d_sq) +
         (2 * intermediate_results.c_ab) +
         (2 * intermediate_results.d * (intermediate_results.sa + intermediate_results.sb))
-    # p_sq = round(p_sq; digits=10)
 
     if p_sq >= 0
         p = sqrt(p_sq)
@@ -85,10 +63,9 @@ function dubins_LSR(intermediate_results::DubinsIntermediateResults{F}) where {F
                     intermediate_results.sb
                 ),
             ) - atan(-2.0, p)
-        # tmp0 = round(tmp0; digits=10)
-        out1 = mod2pi(tmp0 - intermediate_results.α) |> cleanup_loop
+        out1 = cleanup_loop(mod2pi(tmp0 - intermediate_results.α), loop_tol)
         out2 = p
-        out3 = mod2pi(tmp0 - mod2pi(intermediate_results.β)) |> cleanup_loop
+        out3 = cleanup_loop(mod2pi(tmp0 - mod2pi(intermediate_results.β)), loop_tol)
         out = SVector{3,F}(out1, out2, out3)
         return EDUBOK, out
     end
@@ -96,13 +73,12 @@ function dubins_LSR(intermediate_results::DubinsIntermediateResults{F}) where {F
     return EDUBNOPATH, SVector{3,F}(0, 0, 0)
 end
 
-function dubins_RSL(intermediate_results::DubinsIntermediateResults{F}) where {F}
+function dubins_RSL(intermediate_results::DubinsIntermediateResults{F}, loop_tol::F) where {F}
 
 
     p_sq =
         -2 + intermediate_results.d_sq + (2 * intermediate_results.c_ab) -
         (2 * intermediate_results.d * (intermediate_results.sa + intermediate_results.sb))
-    # p_sq = round(p_sq; digits=10)
 
     if p_sq >= 0
         p = sqrt(p_sq)
@@ -114,10 +90,9 @@ function dubins_RSL(intermediate_results::DubinsIntermediateResults{F}) where {F
                     intermediate_results.sb
                 ),
             ) - atan(2.0, p)
-        # tmp0 = round(tmp0; digits=10)
-        out1 = mod2pi(intermediate_results.α - tmp0) |> cleanup_loop
+        out1 = cleanup_loop(mod2pi(intermediate_results.α - tmp0), loop_tol)
         out2 = p
-        out3 = mod2pi(intermediate_results.β - tmp0) |> cleanup_loop
+        out3 = cleanup_loop(mod2pi(intermediate_results.β - tmp0), loop_tol)
         out = SVector{3,F}(out1, out2, out3)
         return EDUBOK, out
     end
@@ -125,7 +100,7 @@ function dubins_RSL(intermediate_results::DubinsIntermediateResults{F}) where {F
     return EDUBNOPATH, SVector{3,F}(0, 0, 0)
 end
 
-function dubins_RLR(intermediate_results::DubinsIntermediateResults{F}) where {F}
+function dubins_RLR(intermediate_results::DubinsIntermediateResults{F}, loop_tol::F) where {F}
 
     tmp0 =
         (
@@ -137,19 +112,14 @@ function dubins_RLR(intermediate_results::DubinsIntermediateResults{F}) where {F
         intermediate_results.ca - intermediate_results.cb,
         intermediate_results.d - intermediate_results.sa + intermediate_results.sb,
     )
-    # tmp0 = round(tmp0; digits=10)
-    # phi = round(phi; digits=10)
 
     if abs(tmp0) <= 1
         p = mod2pi((2 * π) - acos(tmp0))
         t = mod2pi(intermediate_results.α - phi + mod2pi(p / 2.0))
-        # p = round(p; digits=10)
-        # t = round(t; digits=10)
-        out1 = t |> cleanup_loop
-        out2 = p |> cleanup_loop
-        out3 =
-            mod2pi(intermediate_results.α - intermediate_results.β - t + mod2pi(p)) |>
-            cleanup_loop
+        out1 = cleanup_loop(t, loop_tol)
+        out2 = cleanup_loop(p, loop_tol)
+        out3 = mod2pi(intermediate_results.α - intermediate_results.β - t + mod2pi(p))
+        out3 = cleanup_loop(out3, loop_tol)
         out = SVector{3,F}(out1, out2, out3)
         return EDUBOK, out
     end
@@ -157,7 +127,7 @@ function dubins_RLR(intermediate_results::DubinsIntermediateResults{F}) where {F
     return EDUBNOPATH, SVector{3,F}(0, 0, 0)
 end
 
-function dubins_LRL(intermediate_results::DubinsIntermediateResults{F}) where {F}
+function dubins_LRL(intermediate_results::DubinsIntermediateResults{F}, loop_tol::F) where {F}
 
     tmp0 =
         (
@@ -169,20 +139,17 @@ function dubins_LRL(intermediate_results::DubinsIntermediateResults{F}) where {F
         intermediate_results.ca - intermediate_results.cb,
         intermediate_results.d + intermediate_results.sa - intermediate_results.sb,
     )
-    # tmp0 = round(tmp0; digits=10)
-    # phi = round(phi; digits=10)
 
     if abs(tmp0) <= 1
         p = mod2pi(2 * π - acos(tmp0))
         t = mod2pi(-intermediate_results.α - phi + p / 2.0)
-        # p = round(p; digits=10)
-        # t = round(t; digits=10)
-        out1 = t |> cleanup_loop
-        out2 = p |> cleanup_loop
+        out1 = cleanup_loop(t, loop_tol)
+        out2 = cleanup_loop(p, loop_tol)
         out3 =
             mod2pi(
                 mod2pi(intermediate_results.β) - intermediate_results.α - t + mod2pi(p),
-            ) |> cleanup_loop
+            )
+        out3 = cleanup_loop(out3, loop_tol)
         out = SVector{3,F}(out1, out2, out3)
         return EDUBOK, out
     end
